a33a810affd75ea1a9ae3c10b1b1892ce4916757
bustta <bustta80980@gmail.com>
2014-11-11 00:54:26 +0800

    make parser work and add disp result page

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from newsParser import NewsParser
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("ready.html", url=None)
 
-@app.route("/add", methods=['POST'])
+@app.route("/parse", methods=['POST'])
 def execute_parse():
-    pass
+    parse_obj = NewsParser(request.form['newsUrl'])
+    title = parse_obj.getTitle()
+    content = parse_obj.getContent()
+    author = parse_obj.getAuthor()
+    pub_time = parse_obj.getPublishDate()
+    return render_template("show_detail.html",
+        title=title, content=content, author=author, pub_time=pub_time)
+
 
 
 if __name__ == "__main__":
